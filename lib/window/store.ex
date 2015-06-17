748987aca1170805414dd8d140d516439d45976d
Jonathan Harrington <jonathan@jonharrington.org>
2015-06-17 17:03:10 +0100

    Remove commented out code

--- lib/window/store.ex
+++ lib/window/store.ex
@@ -4,7 +4,6 @@ defmodule Window.Store do
   # external API
   def start_link() do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
-    #Agent.start_link(fn -> Map.new end, name: __MODULE__)
   end
 
   def get(id) do
@@ -21,13 +20,11 @@ defmodule Window.Store do
   end
 
   def handle_call({:get, id}, _from, store) do
-    #{:ok, window} = Agent.get(store, &Map.get(&1, id))
     window = Map.get(store, id)
     {:reply, window, store}
   end
 
   def handle_cast({:put, window}, store) do
-    #Agent.update(store, &Map.put(&1, window.id, window))
     store = Map.put(store, window.id, window)
     {:noreply, store}
   end
